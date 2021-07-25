fd1125c6390ac105556610b3551fcb82c58273e4
DJ.Next <87913678+DevPrionTeam@users.noreply.github.com>
2021-07-25 16:31:55 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//############ Very Simple Turn Based Battle System ############//
+//############# Made By : @DevPrionTeam - DJ.Next  #############// 
+//######## It's free to use as base for your projects ;) #######//
+using System;
+using RPGBattleSystem.Sys;
+
+namespace RPGBattleSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Window Setup
+            Console.Title = "Simple and basic RPG Turn Based Battle.";
+            Game game = new Game();// Init Class
+            game.Start();// Start Game.
+        }
+    }
+}
+//###############################################################//
